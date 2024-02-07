18967834f89f9b0b64dc09954577bf3e273ddd9e
Arthur Kellermann <arthur.kellermann956@gmail.com>
2024-02-07 18:55:34 -0300

    feat: add get poll by id route

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,30 @@
+import { z } from "zod"
+import { prismaClient } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function getPoll(app: FastifyInstance) {
+    app.get('/polls/:pollId', async (request, reply) => {
+        const getPollParams = z.object({
+            pollId: z.string().uuid()
+        });
+
+        const { pollId } = getPollParams.parse(request.params);
+
+        const poll = await prismaClient.poll.findUnique({
+            where: {
+                id: pollId
+            },
+            include: {
+                options: {
+                    select: {
+                        id: true,
+                        title: true
+                    }
+                }
+            }
+        });
+
+        return reply.send({ poll });
+    })
+
+}
