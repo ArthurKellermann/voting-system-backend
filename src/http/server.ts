3fef5c194ade379ca673520937c178bf699335bc
Arthur Kellermann <arthur.kellermann956@gmail.com>
2024-02-08 16:41:48 -0300

    feat: add websocket to update votes in real-time

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify';
+import fastifyCookie from '@fastify/cookie';
+import fastifyWebsocket from '@fastify/websocket';
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
-import fastifyCookie from '@fastify/cookie';
+import { pollResult } from './ws/poll-result';
 
 const app = fastify();
 
@@ -11,9 +13,12 @@ app.register(fastifyCookie, {
     hook: 'onRequest'
 });
 
+app.register(fastifyWebsocket);
+
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResult);
 
 app.listen({ port: 3333 }).then(() => {
     console.log("HTTP server running!")
